61da4e547fd4e512536a30228f753ecccaf7a0b5
Akram <elyoubi.akram@gmail.com>
2018-12-15 20:15:07 +0100

    update manifest for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,15 +50,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-87363639-1`,
       },
     },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Akram El Youbi Blog`,
+        short_name: `elyoubi`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
