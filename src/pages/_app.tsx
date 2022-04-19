83f07a350f2e6a082781e525a6be8361d2473c31
youbicode <elyoubi.akram@gmail.com>
2022-04-19 15:31:33 +0200

    add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import PlausibleProvider from "next-plausible";
 
 import theme from "../theme";
 import Layout from "@/components/Layout";
@@ -8,9 +9,11 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <PlausibleProvider domain="elyoubi.com">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PlausibleProvider>
     </ChakraProvider>
   );
 }
